c53f9a7939f428303c1787cbf87fcedc6e89b3f4
=tmorrisiii86 <=tmorrisiii86@gmail.com>
2020-02-18 10:39:59 -0600

    Add project files.

---
+++ Lab2_5_Thomas_Morris/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lab2_5_Thomas_Morris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int numberOne; //user input1
+            int numberTwo; //user input2
+
+            Console.WriteLine("Enter first number value");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number value");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            //adding numbers
+            int results = numberOne + numberTwo;
+            Console.WriteLine("The answer is " + results);
+
+            //subtracting numbers
+            results = numberOne - numberTwo;
+            Console.WriteLine("The answer is " + results);
+
+            //multiplying numbers
+            results = numberOne * numberTwo;
+            Console.WriteLine("The answer is " + results);
+
+            //dividing numbers
+            double results1 = (double)numberOne / (double)numberTwo;
+            double results2 = numberOne % numberTwo;
+            Console.WriteLine("The answer is " + results1);
+            Console.WriteLine("The remainder is " + results2);
+
+            //2
+            string yourName;
+
+            Console.WriteLine("Enter your name");
+            yourName = Console.ReadLine();
+            Console.WriteLine("Hello " + yourName + "!!!");
+
+            //3
+            int numberA;
+            int numberB;
+            int numberC;
+
+            Console.WriteLine("Enter first number");
+            numberA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            numberB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            numberC = Convert.ToInt32(Console.ReadLine());
+
+            int resultD = numberA * numberB * numberC;
+            Console.WriteLine("The answer is " + resultD);
+
+            //4
+            int yourAge;
+            int yourAgeYounger;
+
+            Console.WriteLine("Enter your age");
+            yourAge = Convert.ToInt32(Console.ReadLine());
+            yourAgeYounger = yourAge - 5;
+            Console.WriteLine("You look younger than " + yourAge + 
+                ". Are you sure you are not " + yourAgeYounger +"?");
+        }
+    }
+}
